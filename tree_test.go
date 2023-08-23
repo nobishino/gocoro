7ce6583ded75ef6caaf14451dc20030921c30bdd
nobishino <nobishii11@gmail.com>
2023-08-23 21:23:51 +0900

    Add Pull Example

--- tree_test.go
+++ tree_test.go
@@ -18,6 +18,14 @@ func TestTreeCmp(t *testing.T) {
 	fmt.Println(cmp(t1, t2), cmp(t1, t3))
 }
 
+func TestTreeCmpWithoutPull(t *testing.T) {
+	fmt.Println(
+		cmp_without_pull(t1, t2),
+		cmp_without_pull(t1, t3),
+	)
+
+}
+
 type Tree[V any] struct {
 	Left  *Tree[V]
 	Value V
@@ -37,6 +45,7 @@ func (t *Tree[V]) all(yield func(v V) bool) bool {
 		t.Left.all(yield) && yield(t.Value) && t.Right.all(yield)
 }
 
+// Pullを利用した場合の比較コード
 func cmp[V comparable](t1, t2 *Tree[V]) bool {
 	next1, stop1 := coro.Pull(t1.All)
 	next2, stop2 := coro.Pull(t2.All)
@@ -52,4 +61,35 @@ func cmp[V comparable](t1, t2 *Tree[V]) bool {
 			return true
 		}
 	}
+}
+
+// Pullを利用しない場合の比較コード
+func cmp_without_pull[V comparable](t1, t2 *Tree[V]) bool {
+	resume1 := coro.New(func(_ bool, yield func(V) bool) (zero V) {
+		t1.All(yield)
+		return zero
+	})
+	resume2 := coro.New(func(_ bool, yield func(V) bool) (zero V) {
+		t2.All(yield)
+		return zero
+	})
+	// この処理を省いた場合、coroutineがyieldの戻り値を待ってブロックした状態のままleakする
+	defer func() {
+		z1, ok1 := resume1(false)
+		z2, ok2 := resume1(false)
+		fmt.Println("Comparison finished")
+		fmt.Println(z1, ok1)
+		fmt.Println(z2, ok2)
+	}()
+	for {
+		v1, ok1 := resume1(true)
+		v2, ok2 := resume2(true)
+		if v1 != v2 || ok1 != ok2 {
+			return false
+		}
+		if !ok1 && !ok2 {
+			return true
+		}
+	}
+
 }
